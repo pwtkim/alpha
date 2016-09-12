3e9c02afaae4d5e6e5aa94917522d3bf6963ebfe
Peter Kim <pwtkim@gmail.com>
2016-09-11 19:16:10 -0500

    fix test

--- client/web-client/src/app/app.component.spec.ts
+++ client/web-client/src/app/app.component.spec.ts
@@ -3,9 +3,15 @@
  */
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MdToolbarModule } from "@angular2-material/toolbar";
+import { MdSidenavModule } from "@angular2-material/sidenav";
+
 describe('App', () => {
     beforeEach(() => {
-        TestBed.configureTestingModule({ declarations: [AppComponent]});
+        TestBed.configureTestingModule({
+            imports: [MdToolbarModule, MdSidenavModule],
+            declarations: [AppComponent]
+        });
     });
     it ('should work', () => {
         let fixture = TestBed.createComponent(AppComponent);
